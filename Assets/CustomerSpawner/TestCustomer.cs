f96a4e06a75b02ac0ef4caa21158f49704e157b9
Catbugpig <68763221+Catbugpig@users.noreply.github.com>
2020-07-26 15:04:35 -0400

    Add files via upload
    The Customer spawn.

---
+++ Assets/CustomerSpawner/TestCustomer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCustomer : MonoBehaviour {
+
+	float speed = 2f;
+
+	void Update () 
+	{
+		transform.Translate(speed * Time.deltaTime, 0, 0);
+
+		Destroy(gameObject, 3f);
+	}
+}
