8b65030cce14e565996c900eed71020a87640365
Song Yang <songyang1218@gmail.com>
2020-07-28 00:05:00 -0400

    Add left clicking to selected people

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Handles user controls
+/// </summary>
+public class GameManager : MonoBehaviour
+{
+    [Tooltip("Material of a selected person")]
+    public Material SelectedMaterial;
+
+    // Update is called once per frame
+    void Update()
+    {
+        LeftClickListener();
+    }
+
+    private void LeftClickListener()
+    {
+        // left click up
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+            if (hit.collider != null)
+            {
+                switch (hit.collider.gameObject.name)
+                {
+                    case "Customer":
+                    case "Employee":
+                        
+                        hit.collider.GetComponentInChildren<SpriteRenderer>().material = SelectedMaterial;
+                        
+                        break;
+
+                    case "Tables":
+                        break;
+                }
+            }
+        }
+    }
+}
