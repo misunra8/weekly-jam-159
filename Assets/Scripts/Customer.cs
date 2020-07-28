d6bfc1ba8fecf8dc67698f1e72b9feb7411a6bac
Song Yang <songyang1218@gmail.com>
2020-07-28 17:38:03 -0400

    Reset Customer script

--- Assets/Scripts/Customer.cs
+++ Assets/Scripts/Customer.cs
@@ -11,61 +11,22 @@ public class Customer : Person
     /// <summary>
     /// Reference to the customer behavior state machine
     /// </summary>
-    public TileBase CashRegister;
-
-    float themeRTPC = 80;
-
+    private Animator behavior;
+    
     /// <summary>
     /// Assigns references
     /// </summary>
-    /// <param name="other"></param>
-    /// 
-
-    private void Start() {
-        AkSoundEngine.PostEvent("ThemePlay", gameObject);
-        AkSoundEngine.SetRTPCValue("Theme_RTPC", themeRTPC);
-    }
-    private void OnCollisionEnter2D(Collision2D other)
+    private void Start()
     {
-   
-        
-
-        switch (other.gameObject.name)
-        {
-            // colliding with something in the tables tilemap
-            case "Tables":
-                CollideTable(other);
-                break;
-        }
+        behavior = GetComponent<Animator>();
     }
     
     /// <summary>
     /// Sits at the table, influences the behavior machine
     /// </summary>
-    /// <param name="other">Unity's detected collision</param>
-    private void CollideTable(Collision2D other)
+    /// <param name="cell"></param>
+    protected override void ActOnTableCollision(Vector3Int cell)
     {
         
-        var tablesTilemap = other.gameObject.GetComponent<Tilemap>();
-        var contact = other.GetContact(0); // first contact point
-        
-        // To get the tile, we need to get a world position inside a cell.
-        // But the collision position is not inside the cell all the time.
-        // So we'll lengthen the path taken by the customer by a step to get a
-        // position inside the cell.
-        
-        // Find the position that is slightly ahead in the customer
-        // Use the opposite direction of the normal of the surface of collision 
-        var oppositeNormal = contact.normal.normalized * -1;
-
-        var simulatedPosition = oppositeNormal * CollisionStep + contact.point;
-        var cell = tablesTilemap.WorldToCell(simulatedPosition);
-
-
-        // Change the touched tile to another tile
-        tablesTilemap.SetTile(cell, CashRegister);
-        float themeRTPC = 40;
-        AkSoundEngine.SetRTPCValue("Theme_RTPC", themeRTPC);
-        AkSoundEngine.PostEvent("CoinPay", gameObject);
     }
 }
